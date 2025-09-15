5ff5acd43bc38ef3837069b66c6bfccb003ee505
eliveltoncosta014 <eliveltonjob@gmail.com>
2025-09-15 14:58:43 -0300

    feat adicionando novo arquivo

--- JavaScript/niver.js
+++ JavaScript/niver.js
@@ -4,10 +4,28 @@ function diasParaNiver(){
     const diaSemana = hoje.getDay();// 0=Dom, 1=seg, ...,4=quin
     let diasFaltando = 4-diaSemana;
 
-    if(diasFaltando <0 ) {
+    if(diasFaltando <0 ) 
         diasFaltando += 7;
-    }
+    
     document.getElementById ("dias"). textContent= diasFaltando;
 }
+function atualizarhora(){
+    const horaHtml= document.getElementById("hora");
+    const agora= new Date();
+    const hora= agora.getHours();
+    const minutos= agora.getMinutes();
+    const segundos= agora.getSeconds();
+
+    horaHtml.textContent = `${hora}:${minutos}:${segundos}`;
+
+
+}
+diasParaNiver();
+
+// chamar funções 
 
 diasParaNiver();
+
+setInterval(atualizarhora, 1000);
+
+atualizarhora();
