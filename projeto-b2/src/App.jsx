8e8e05d520c521e94ecf8541ef837265cb059be2
eliveltoncosta014 <eliveltonjob@gmail.com>
2025-09-19 15:05:25 -0300

    feat: add

--- projeto-b2/src/App.jsx
+++ projeto-b2/src/App.jsx
@@ -1,17 +1,13 @@
-
+import Contador from "./componentes/Contador"
 
 function App() {
-  
+
 
   return (
     <>
-   <h1>Olá Rodringuinho 🎂🎆</h1>
-    <h2>Giovanni</h2>
-    <h3>parabéns pelo seu aniversario!</h3>
-     <h4>ano qu vem tem mais </h4>
-      <h5>muita saude</h5>     
-      <h6>que voce encontre um bom emprego 🙏</h6>
-      </>
+      <Contador></Contador>
+      
+    </>
   )
 }
 
